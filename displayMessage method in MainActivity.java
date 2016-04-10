a678f447ba6f276b828634925a09064ceb70ac02
VR <symbolic@users.noreply.github.com>
2016-04-10 16:24:54 +0800

    Rename New method in MainActivity.java to displayMessage method in MainActivity.java

---
+++ displayMessage method in MainActivity.java
@@ -0,0 +1,7 @@
+/**
+ * This method displays the given text on the screen.
+ */
+private void displayMessage(String message) {
+    TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+    priceTextView.setText(message);
+}
